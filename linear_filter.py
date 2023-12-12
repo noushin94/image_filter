03a10ea743bca4dc1dbd1f8a04e9c88bbf044d01
Noushin <96178032+noushin94@users.noreply.github.com>
2023-12-12 12:51:44 +0000

    Delete linear_filter.py

--- linear_filter.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import scipy.ndimage
-
-
-# opening the image using cv2
-a = cv2.imread('image2/11.jpg')
-# converting the image to grayscale
-a = cv2.cvtColor(a,cv2.COLOR_BGR2GRAY)
